e1af31877f2e26555cb66e102d9240a2a2d2b0f1
devhulk <gerald.yerden@gmail.com>
2022-12-07 12:37:52 -0500

    added download of files

--- node-image-gen/index.js
+++ node-image-gen/index.js
@@ -4,13 +4,36 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+var fs = require('fs'),
+    request = require('request');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+
 const makeCall = async () => {
+    let urls = []
     const response = await openai.createImage({
       prompt: "A cute baby sea otter",
       n: 2,
       size: "1024x1024",
     });
-    console.log(response.data)
+
+    response.data.data.forEach((image, index) => {
+        console.log(image.url)
+        download(image.url, `image${index}.png`, function(){
+            console.log('done')
+        })
+
+    })
+
+
 
     return response
 }
